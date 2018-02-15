57ab532e8c1a1f05f884fac95fda67ab2307a4f8
Ricardo van der Vlag <ricadovandervlag@gmail.com>
2018-02-15 13:19:23 +0100

    Added comments to Encrypt.java

--- java/src/Classes/Encrypt.java
+++ java/src/Classes/Encrypt.java
@@ -2,12 +2,18 @@ package Classes;
 import java.util.ArrayList;
 import java.util.Collections;
 
+// @author:     Ricardo van der Vlag
+// @version:    2018-2-15
 public class Encrypt {
+    // Generate a key for encryption
     public ArrayList generateKey(){
+        // Create ArrayList which will hold the key information
         ArrayList key = new ArrayList();
+        // Fill key with 26 entries represending the number of the alphabet e.g. a = 0, b = 1 etc.
         for (int i = 0; i < 26; i++){
             key.add(i);
         }
+        // Shuffle key randomly
         Collections.shuffle(key);
         return key;
     }
