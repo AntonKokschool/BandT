7d5185beb1d6958955c78542bb6dcf901eac61bc
Ricardo van der Vlag <ricadovandervlag@gmail.com>
2018-02-15 13:26:39 +0100

    Removed unused imports

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,12 +1,4 @@
-
-
 import Classes.ReadFile;
-import com.sun.xml.internal.bind.v2.model.core.NonElement;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 // Main class
 public class Main {
