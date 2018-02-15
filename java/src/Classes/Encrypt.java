b3666775adb47da7c909c71c13f3adc46bc50c10
Ricardo van der Vlag <ricadovandervlag@gmail.com>
2018-02-15 15:47:43 +0100

    Added encryption

--- java/src/Classes/Encrypt.java
+++ java/src/Classes/Encrypt.java
@@ -4,11 +4,36 @@ import java.util.Random;
 // @author:     Ricardo van der Vlag
 // @version:    2018-2-15
 public class Encrypt {
-    // Generate a key for encryption
-    public int generateKey(){
+    // Max value for key is max number of extended ascii
+    private int max = 255;
+    // Generate a random key for encryption
+    private int generateKey(){
         Random rand = new Random();
-        int key = rand.nextInt(26) + 1;
-
+        // Max value = 254 min value = 1
+        int key = rand.nextInt(max - 2) + 1;
         return key;
+    }
+
+    // Encrypt file using key
+    public String encryptFile(String file){
+        // Generate a key
+        int key = generateKey();
+        // Show private key
+        System.out.println("The private key is: " + key);
+        // Prepare String for returning
+        String ret = "";
+        // Read every character
+        for (int i = 0; i < file.length(); i++){
+            // Define ascii number of current character
+            int ascii = (int) file.charAt(i);
+            // Raise current ascii number with private key
+            ascii = ascii + key;
+            // If the new ascii number is higher than the max number of ascii,
+            if (ascii > max){
+                ascii = ascii - (max + 1);
+            }
+            ret = ret + Character.toString((char)ascii);
+        }
+        return ret;
     }
 }
